3d8be4144e28892861ed103e95aa61d59763f042
Gustavo Pertile <gu.follador@gmail.com>
2021-10-08 15:52:35 -0300

    Upando projeto de temporizador

---
+++ js/temporizador.js
@@ -0,0 +1,54 @@
+(function ($) {
+    $.fn.temporizador = function (opcoes) {
+        const opcoesFinais = $.extend({
+            mensagem: 'Em breve!',
+            horario: '23:59:59'
+        }, opcoes)
+
+        const spanDigito = '<span class="digito">'
+
+        const horaDezena = $(spanDigito).html('0')
+        const horaUnidade = $(spanDigito).html('0')
+        const minutoDezena = $(spanDigito).html('0')
+        const minutoUnidade = $(spanDigito).html('0')
+        const segundoDezena = $(spanDigito).html('0')
+        const segundoUnidade = $(spanDigito).html('0')
+
+        const separadorHora = $('<span class="separador">').html(':')
+        const separadorMinuto = $('<span class="separador">').html(':')
+        const mensagem = $('<div class="mensagem">').html(opcoesFinais.mensagem)
+
+        $(this).addClass('temporizador')
+        $(this).append(horaDezena, horaUnidade, separadorHora,
+            minutoDezena, minutoUnidade, separadorMinuto,
+            segundoDezena, segundoUnidade, mensagem)
+
+        const regex = new RegExp(/(\d\d):(\d\d):(\d\d)/)
+        const horarioAlvo = regex.exec(opcoesFinais.horario)
+        // console.log(horarioAlvo)
+
+        let temporizador = setInterval(() => {
+            const agora = new Date()
+            const alvo = new Date()
+            alvo.setHours(horarioAlvo[1])
+            alvo.setMinutes(horarioAlvo[2])
+            alvo.setSeconds(horarioAlvo[3])
+
+            const diferencaEmMili = alvo.getTime() - agora.getTime()
+            if(diferencaEmMili >= 0) {
+                const diferenca = regex.exec(new Date(diferencaEmMili).toISOString())
+                // console.log(diferenca)
+
+                horaDezena.html(diferenca[1][0])
+                horaUnidade.html(diferenca[1][1])
+                minutoDezena.html(diferenca[2][0])
+                minutoUnidade.html(diferenca[2][1])
+                segundoDezena.html(diferenca[3][0])
+                segundoUnidade.html(diferenca[3][1])
+            } else {
+                clearInterval(temporizador)
+            }
+        }, 1000)
+        return this
+    }
+})(jQuery)
